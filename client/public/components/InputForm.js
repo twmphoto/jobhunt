ae878c7a503cfdbe6e5280e5a41864715fe7916c
twmphoto <twmphoto@icloud.com>
2018-10-18 14:59:25 -0700

    New Pages Created

---
+++ client/public/components/InputForm.js
@@ -0,0 +1,20 @@
+class InputForm extends Component {
+    state = {
+      username: '',
+      email: '',
+      password: '',
+      confirm: '',
+      badEntry: false
+    }
+  
+    
+    render() { 
+        return (
+            <div>This is the InputForm Component</div>
+
+
+          );
+    }
+}
+ 
+export default InputForm;
---
+++ client/public/components/SignUp.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+
+class SignUp extends Component {
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        confirm: '',
+        badEntry: false
+    }
+
+    submitUser = () => {
+        let { username, email, password, confirm } = this.state;
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <div>This is the SignUp Component</div>
+                <form>username
+                    <TextInput 
+                        textAlign = 'center'
+                        onChangeText = {}></TextInput>
+
+
+
+                </form>
+            </div>
+
+        );
+    }
+}
+
+export default SignUp;
