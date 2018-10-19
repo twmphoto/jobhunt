f3b488701cd450540254788f982682f46a6a8409
twmphoto <twmphoto@icloud.com>
2018-10-18 20:57:10 -0700

    Signup form revised

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
 import HomePage from '../src/components/homepage/homepage';
 import './App.css';
-import {Router, Route, Switch} from 'react-router';
-import {Link} from 'react-router-dom';
-import Forms from '../src/components/forms/Forms';
-import Signup from '../src/components/signup/Signup';
-import Dashboard from '../src/components/dashboard/dashboard';
-import Homepage from '../src/components/homepage/homepage';
 
 
 class App extends Component {
-  state = {
-    foo: '',
-    bar: ''
-  }
+  
   render() {
     return (
       <div className="App">
         <HomePage/>
-      <div>
-        <Link to="/">Go Back</Link>
-        <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/forms/' render={(renderProps) => <Forms {...renderProps} />} />
-          <Route path='/signup/' render={(renderProps) => <Signup {...renderProps} />} />
-          <Route path='/dashboard/' render={(renderProps) => <Dashboard {...renderProps} />} />
-        </Switch>
-      </div>
       </div>
     );
   }
--- client/src/components/forms/Forms.js
+++ client/src/components/forms/Forms.js
@@ -6,7 +6,7 @@ class Forms extends Component {
     state = {
       companyName: '',
       linkToJob: '',
-      status: '',
+      jobStatus: '',
       notes: ''
     }
   
@@ -17,56 +17,55 @@ class Forms extends Component {
         })
     }
 
-    
+    submitUser = () => {
+        
+    }
+
     render() { 
         return (
             <div>
                 <div>This is the Job Forms Component</div>
                 <form onSubmit={this.submitUser}>
-                    <label>Company Name</label>
+                    <label className="title">Company Name</label>
+                    <input
+                        onChange = {this.handleOnChange}
+                        value="companyName"
+                        name="companyName"
+                        
+                    />
+                    <label className="title">Job Prospects</label>
                     <input
                         textAlign='center'
                         onChange = {this.handleOnChange}
-                        value={companyName}
-                        name={companyName}
+                        value="linkToJob"
+                        name="linkToJob"
                         
                     />
-                    <label>Job Prospects</label>
-                    <input
-                        textAlign='center'
-                        onChange = {this.handleOnChange}
-                        value={linkToJob}
-                        name={linkToJob}
-                        
-                    />
-                    <label>Current Status</label>
-                    <select value={this.state.value} onChange={this.handleChange} value={status}>
-                    <option selected value = ''>Please Select</option>
-                    <option value='0'>Call Back</option>
-                    <option value='1'>Phone Interview</option>
-                    <option value='2'>In Person 1</option>
-                    <option value='3'>In Person 2</option>
-                    <option value='4'>In Person 3</option>
-                    <option value='5'>Offer Received</option>
+                    <label>
+                    Current Status
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option selected value = ''>Please Select</option>
+                        <option value='0'>Call Back</option>
+                        <option value='1'>Phone Interview</option>
+                        <option value='2'>In Person 1</option>
+                        <option value='3'>In Person 2</option>
+                        <option value='4'>In Person 3</option>
+                        <option value='5'>Offer Received</option>
                     </select>
+                    </label>
                     
                     <label>Notes</label>
                     <textarea
                         textAlign='center'
                         onChange = {this.handleOnChange}
                         placeholder="Notes about..."
-                        value={notes}
-                        name={notes}
+                        value='notes'
+                        name='notes'
                         style={{ width: "200px" }}
                     />
                 
                 </form>
             </div>
-
-            
-
-
-
           );
     }
 }
