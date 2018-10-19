db83ac31611039ca297fc1e5d2fdf7f72e9f9cc7
twmphoto <twmphoto@icloud.com>
2018-10-18 17:06:39 -0700

    User Sign Up

---
+++ client/public/components/Forms.js
@@ -0,0 +1,19 @@
+class Forms extends Component {
+    state = {
+      companyName: '',
+      linkToJob: '',
+      status: '',
+      notes: ''
+    }
+  
+    
+    render() { 
+        return (
+            <div>This is the Jobs FormComponent</div>
+
+
+          );
+    }
+}
+ 
+export default Forms;
--- client/public/components/SignUp.js
+++ client/public/components/SignUp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 
 
@@ -16,15 +17,70 @@ class SignUp extends Component {
 
     }
 
+    handleSubmit = e => {
+        event.preventDefault();
+        alert('User Form Submitted ');
+    }
+
+    handleOnChange = e => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    confirmPass = e =>{
+        if (e.target.name === this.state.password){
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        } else 
+            alert('Passwords do not match')
+            
+            
+
+
+    }
+
 
     render() {
         return (
             <div>
                 <div>This is the SignUp Component</div>
-                <form>username
-                    <input 
-                        textAlign = 'center'
-                        onChangeText = {}></input>
+                <form onSubmit={this.submitUser}>
+                    <label>Username</label>
+                    <input
+                        textAlign='center'
+                        onChange = {this.handleOnChange}
+                        value={username}
+                        name={username}
+                        style={textInput}
+                    />
+                    <label>Email</label>
+                    <input
+                        textAlign='center'
+                        onChange = {this.handleOnChange}
+                        value={email}
+                        name={email}
+                        style={textInput}
+                    />
+                    <label>Password</label>
+                    <input
+                        textAlign='center'
+                        onChange = {this.handleOnChange}
+                        value={password}
+                        name={password}
+                        style={textInput}
+                    />
+                    <label>Confirm Password</label>
+                    <input
+                        textAlign='center'
+                        onChange = {this.confirmPass}
+                        value={confirm}
+                        name={confirm}
+                        style={textInput}
+                    />
+                    
 
 
 
